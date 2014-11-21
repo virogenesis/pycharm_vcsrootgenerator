5579ea8740b875c448283952bc9e517a3ef7d23d
Bojan Jovanovic <bojan@gregorllc.com>
2014-11-21 16:15:37 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import glob
 from distutils.core import setup
+from setuptools import find_packages
+
 
 setup(
     name='pycharm_vcsrootgenerator',
     version='0.1',
-    packages=[],
+    packages=find_packages(),
     install_requires=[
         'lxml',
     ],
